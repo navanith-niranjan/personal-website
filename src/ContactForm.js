8f6fd8e4dc3b0d62c10ecf4c978dd00f5e3868f0
Navanith Niranjan <navanith.niran@gmail.com>
2023-06-12 20:15:02 -0400

    netlify form fix

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,7 +1,7 @@
 const ContactForm = () => {
 
     return (  
-        <form name="contact" method="POST" onSubmit="submit" netlify>
+        <form name="contact" method="POST" onSubmit="submit">
             <input type="hidden" name="form-name" value="contact" />
                 <div className="WholeForm">
                     <div className="FormContents">
@@ -27,7 +27,7 @@ const ContactForm = () => {
                         </div>
                         <button type="submit" className="send">send<span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M10 17V7l5 5l-5 5Z"/></svg></span></button>
                     </div>
-                    </div>
+                </div>
         </form>
     );
 }
